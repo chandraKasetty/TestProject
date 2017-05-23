fc78593721a7ee572b7e293d2f6717e748654735
chandraKasetty <ckasetty@gmail.com>
2017-05-23 13:56:47 +0100

    Update RouteConfig.cs

--- Ap16WebApplication/Ap16WebApplication/App_Start/RouteConfig.cs
+++ Ap16WebApplication/Ap16WebApplication/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Ap16WebApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Task", action = "ShowTasks", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
